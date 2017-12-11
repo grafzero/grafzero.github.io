81dfcb0649a6dc3001ec811ce894f944392fd4a1
slotwa <maciekslotwinski@gmail.com>
2017-12-11 15:06:57 +0100

    fixes

--- js/main.js
+++ js/main.js
@@ -34,12 +34,60 @@ $('article.project').click (function(data){
 
 //project close
 $('span.close').click (function(){
-    var activeProject  = $('.project_full.active').first();
-    activeProject.toggle(projectToggleDuration);
+    var activeProjects  = $('.project_full.active');
+    activeProjects.toggle(projectToggleDuration);
+    activeProjects.removeClass("active");
 
     // enable scrolling on page when project is closed
     $('body').css('overflow-y','scroll');
 });
+
+
+
+//menu position chosen
+$('ul.nav-ul li[data-item]').click (function(){
+    window.scrollTo(0, 0);
+    $('.btn').toggleClass('open');
+    $('.navigation').toggleClass('visible');
+    $('.navigation-menu').toggleClass('visible');
+    $('body').toggleClass('nav-visible');
+    $('.name-intro').toggleClass('name-intro-menu-layout');
+    $('.layer').toggleClass('zoomed');
+
+    var projectId = '#project' + $(this).data('item');
+    $(projectId).toggle(projectToggleDuration);
+    $(projectId).addClass('active');
+
+    //scroll to section with project when project is clicked
+    $("html, body").animate({ scrollTop: $('#projects').offset().top }, 1000);
+
+    //disable scroling when project is displayed
+    $('body').css('overflow','hidden');
+});
+
+$('ul.nav-ul li#contactLink').click (function(){
+    window.scrollTo(0, 0);
+    $('.btn').toggleClass('open');
+    $('.navigation').toggleClass('visible');
+    $('.navigation-menu').toggleClass('visible');
+    $('body').toggleClass('nav-visible');
+    $('.name-intro').toggleClass('name-intro-menu-layout');
+    $('.layer').toggleClass('zoomed');
+
+
+    //scroll to section with project when project is clicked
+    $("html, body").animate({ scrollTop: $('#contacts').offset().top }, 1000);
+
+
+});
+
+
+// localization to pl
+$('div.language.pl.transparent').click(function(){
+    $("[data-localize]").localize("slotwa");
+});
+
+
 
 
 
