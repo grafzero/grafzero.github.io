9cb78cbc300198cc7ceb13f293033c42073486d9
slotwa <maciekslotwinski@gmail.com>
2017-12-15 11:39:54 +0100

    responsive fixes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,58 @@
 // svg preloader
 $(window).on('load', function(){
-    $('#preloader').fadeOut();
+    // var now = new Date().getTime();
+    // var page_load_time = now - performance.timing.navigationStart;
+    // console.log("User-perceived page loading time: " + page_load_time);
+var preloader = $('polygon');
+    var polygonsFill = [];
+    var polygons = $('svg polygon');
+    polygons.each(function(){
+        polygonsFill.push($(this).attr("fill"));
+        $(this).removeAttr("fill");
+        console.log('Removing fill!');
+       // $('#preloader').hide().show(0);
+        preloader[0].style.display='none';
+        preloader[0].offsetHeight; // no need to store this anywhere, the reference is enough
+        preloader[0].style.display='block';
+
+
+    });
+
+
+    polygons.each(function(index,element){
+        addFill(index,element);
+        $('#preloader').first().hide().show(0);
+        sleep(10);
+    });
+
+
+    function addFill(polygonIndex, polygon){
+        polygon.setAttribute("fill", "rgb(15,32,43)");
+
+    }
+
+
+
+
+
+
+
+
+
+    $('#preloader').delay(1000).fadeOut();
+
+
+    function sleep(milliseconds) {
+        var start = new Date().getTime();
+        for (var i = 0; i < 1e7; i++) {
+            if ((new Date().getTime() - start) > milliseconds){
+                break;
+            }
+        }
+    }
+
+
+
 });
 
 
@@ -19,13 +71,13 @@ $('.burger-wrapper').click (function(){
 var projectToggleDuration = 500;
 
 //project show
-$('article.project').click (function(data){
+$('article.project:not(.inactive)').click (function(data){
     var projectId = '#project' + $(this).data('item');
     $(projectId).toggle(projectToggleDuration);
     $(projectId).addClass('active');
 
-    //scroll to section with project wjen project is clicked
-    $("html, body").animate({ scrollTop: $('#projects').offset().top }, 1000);
+    //scroll to section with project when project is clicked
+   // $("html, body").animate({ scrollTop: $('#projects').offset().top }, 1000);
 
     //disable scroling when project is displayed
     $('body').css('overflow','hidden');
